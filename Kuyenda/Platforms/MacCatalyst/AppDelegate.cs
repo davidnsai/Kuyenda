0535ab27e8428c5ad406ed9d5e4122f518f9f0af
davidnsai <nsaidave@gmail.com>
2025-07-03 01:36:05 +0200

    Added blank .NET MAUI application

---
+++ Kuyenda/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Kuyenda
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
